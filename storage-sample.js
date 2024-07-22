b49ae680af4373e9a58a1323b022d34c0673b603
kiran varma <mkiranvarma123@gmail.com>
2024-07-22 21:06:45 +0530

    implemented dropdown from entered data

--- storage-sample.js
+++ storage-sample.js
@@ -1,13 +1,31 @@
-const input = document.querySelector('input');
-const names = document.getElementById('dropdown');
-input.addEventListener('click', function() {
-    names.style.display = 'block';
-})
+// const input = document.querySelector('input');
+// const names = document.getElementById('dropdown');
+// input.addEventListener('click', function() {
+//     names.style.display = 'block';
+// })
 
-const h3 = document.querySelector('h3');
-const button = document.querySelector('button');
-button.addEventListener('click', async function(event) {
+// const h3 = document.querySelector('h3');
+// const button = document.querySelector('button');
+// button.addEventListener('click', async function(event) {
+//     event.preventDefault();
+//     h3.innerHTML = 'USA';
+
+// })
+
+
+
+const inputValues = document.querySelector('input');
+const parent = document.getElementById('parent');
+
+
+document.querySelector('button').addEventListener('click',function (event) {
     event.preventDefault();
-    h3.innerHTML = 'USA';
 
+    let child = document.createElement('p');
+    for(let i=1; i<10; i++){
+        localStorage.setItem('City Name',inputValues.value);
+        let value = localStorage.getItem('City Name');
+        child.innerHTML = value;
+    }
+    parent.appendChild(child);
 })
