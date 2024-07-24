b0af2f31c9fbdb4be63d04cfb65f3127300a95b8
kiran varma <mkiranvarma123@gmail.com>
2024-07-24 16:09:04 +0530

    nothing

--- storage-sample.js
+++ storage-sample.js
@@ -14,27 +14,39 @@
 
 
 
-const inputValues = document.querySelector('input');
-const parent = document.getElementById('parent');
+// const inputValues = document.querySelector('input');
+// const parent = document.getElementById('parent');
 
 
-document.querySelector('button').addEventListener('click',function (event) {
-    event.preventDefault();
+// document.querySelector('button').addEventListener('click',function (event) {
+//     event.preventDefault();
 
-    let child = document.createElement('p');
-    for(let i=1; i<10; i++){
-        localStorage.setItem('City Name',inputValues.value);
-        let value = localStorage.getItem('City Name');
-        child.innerHTML = value;
-    }
-    inputValues.value='';
-    parent.appendChild(child);
-    parent.style.display = 'none';
-    parent.classList.add('parentDropDown');
-})
+//     let child = document.createElement('p');
+//     for(let i=1; i<10; i++){
+//         localStorage.setItem('City Name',inputValues.value);
+//         let value = localStorage.getItem('City Name');
+//         child.innerHTML = value;
+//     }
+//     inputValues.value='';
+//     parent.appendChild(child);
+//     parent.style.display = 'none';
+//     parent.classList.add('parentDropDown');
+// })
 
-inputValues.addEventListener('click', function(event) {
-    event.preventDefault();
-    parent.style.display = 'block';
-})
+// inputValues.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     parent.style.display = 'block';
+// })
+
+
+
+const cityName = "New York";
+
+const API = "078092d86e224e14a642a355262f0195";
+
+
+const url = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API}`)
+             .then(response => response.json().then(result => console.log(result)));
+
+
 
