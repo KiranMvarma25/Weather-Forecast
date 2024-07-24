f5466239e64a16e363a2de512f495eaf260b7f82
kiran varma <mkiranvarma123@gmail.com>
2024-07-24 16:57:09 +0530

    checked with forecast url

--- storage-sample.js
+++ storage-sample.js
@@ -45,10 +45,17 @@ const cityName = "New York";
 const API = "078092d86e224e14a642a355262f0195";
 
 
-const url = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API}`)
+const url = fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName} &appid=${API}`)
              .then(response => response.json().then(result => console.log(result)));
 
-console.log(url);
+// const cityName = "New York";
+
+// const API = "078092d86e224e14a642a355262f0195";
+
+
+// const url = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API}`)
+//             .then(response => response.json().then(result => console.log(result)));
+
 
 
 
